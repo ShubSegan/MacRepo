048dc86be14b50a9716961482541e6cb96912fed
ShubSegan <mr.ssegan@gmail.com>
2020-12-21 22:45:48 +0530

    test push 2

--- mobile-app-ws/src/main/java/com/babayaga/MobileAppWsApplication.java
+++ mobile-app-ws/src/main/java/com/babayaga/MobileAppWsApplication.java
@@ -8,7 +8,7 @@ public class MobileAppWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
-		//test push
+		//test push2
 	}
 
 }
